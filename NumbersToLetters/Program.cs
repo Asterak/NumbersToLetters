913905a8c1940bdbb8096d54e9888f60a4ebba67
Сергей Гупалов <asterak@gmail.com>
2020-02-23 12:20:51 +0300

    Add project files.

---
+++ NumbersToLetters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumbersToLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
